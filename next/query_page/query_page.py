7fd783ff475d2a6faadbf2e310a8357f44a4ee0d
Liam <liam@cpan.org>
2017-10-19 23:51:27 -0500

    query_page.py: pass a TargetManager instance into templates

--- next/query_page/query_page.py
+++ next/query_page/query_page.py
@@ -3,8 +3,9 @@ import os
 from flask import Blueprint, render_template, flash, request, redirect, url_for
 
 import next.constants as constants
-from next.api.resource_manager import ResourceManager
+from next.api.resource_manager import ResourceManager, db
 import next.utils as utils
+from next.apps.SimpleTargetManager import SimpleTargetManager
 
 resource_manager = ResourceManager()
 query_page = Blueprint('query_page',
@@ -16,6 +17,8 @@ query_page = Blueprint('query_page',
 @query_page.route('/query_page/<page>/<exp_uid>')
 def load_page(page, exp_uid=None):
     experiment = resource_manager.get_experiment(exp_uid)
+    target_manager = SimpleTargetManager(db)
+
     app_template = page+'.html'
     if constants.NEXT_BACKEND_GLOBAL_HOST:
         host_url = 'http://{}:{}'.format(constants.NEXT_BACKEND_GLOBAL_HOST,
@@ -26,9 +29,9 @@ def load_page(page, exp_uid=None):
     part_id = request.args.get('participant', None)
     participant_uid = str(part_id) if part_id else part_id
 
-        
+
     return render_template(app_template, host_url=host_url, exp_uid=exp_uid,
                            experiment=experiment,
-                           participant_uid=participant_uid), \
+                           participant_uid=participant_uid,
+                           target_manager=target_manager), \
         200, {'Cache-Control':'private, max-age=0, no-cache, no-store'}
-
