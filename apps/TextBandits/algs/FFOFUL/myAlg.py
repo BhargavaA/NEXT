7fecf20303eeecc291a07f9f64d2b750ef1bf858
Aniruddha <threepwood.galahad@gmail.com>
2017-11-16 13:26:44 -0600

    text bandit new files

---
+++ apps/TextBandits/algs/FFOFUL/myAlg.py
@@ -0,0 +1,119 @@
+import time
+import numpy.random as ra
+import numpy as np
+import next.utils as utils
+
+
+class MyAlg:
+    def initExp(self, butler, n, d, failure_probability):
+        params = butler.algorithms.get(key='params')
+        R = params[u'R']
+        S = params[u'S']
+        ridge = params[u'ridge']
+
+        butler.algorithms.set(key='n', value=n)
+        butler.algorithms.set(key='d', value=d)
+        butler.algorithms.set(key='R', value=R)
+        butler.algorithms.set(key='S', value=S)
+        butler.algorithms.set(key='ridge', value=ridge)
+        butler.algorithms.set(key='delta', value=failure_probability)
+
+        if butler.dashboard.get(key='plot_data') is None:
+            butler.dashboard.set(key='plot_data', value=[])
+
+        return True
+
+    def getQuery(self, butler, participant_uid):
+        arm_order = butler.participants.get(uid=participant_uid, key='arm_order')
+        do_not_ask = butler.participants.get(uid=participant_uid, key='do_not_ask')
+
+        num_return = 16
+        counter = 0
+        return_arms = []
+        for next_arm in arm_order:
+            if next_arm not in do_not_ask and next_arm not in return_arms:
+                counter += 1
+                return_arms.append(next_arm)
+                if counter >= num_return:
+                    break
+
+        # butler.participants.append(uid=participant_uid, key='do_not_ask', value=next_arm)
+        return return_arms
+
+    def processAnswer(self, butler, arm_id, reward, num_responses, init_id, participant_uid):
+        if num_responses == 1:
+            d = butler.algorithms.get(key='d')
+            n = butler.algorithms.get(key='n')
+            ridge = butler.algorithms.get(key='ridge')
+            invVt = np.eye(d) / ridge
+            b = np.zeros(d)
+            x_invVt_norm = np.ones(n) / ridge
+
+            butler.participants.set(uid=participant_uid, key='received_rewards', value=[])
+            butler.participants.set(uid=participant_uid, key='invVt', value=invVt)
+            butler.participants.set(uid=participant_uid, key='b', value=b)
+            butler.participants.set(uid=participant_uid, key='x_invVt_norm', value=x_invVt_norm)
+
+        butler.participants.append(uid=participant_uid, key='do_not_ask', value=arm_id)
+
+        butler.participants.append(uid=participant_uid, key='received_rewards', value=reward)
+        butler.participants.increment(uid=participant_uid, key='num_reported_answers')
+
+        update_plot_data = {'rewards': reward,
+                            'participant_uid': participant_uid,
+                            'initial_arm': init_id,
+                            'arm_pulled': arm_id,
+                            'alg': 'TS',
+                            'time': num_responses
+                            }
+
+        butler.dashboard.append(key='plot_data', value=update_plot_data)
+
+        task_args = {
+            'arm_id': arm_id,
+            'reward': reward,
+            'participant_uid': participant_uid
+        }
+
+        butler.job('modelUpdate', task_args, ignore_result=True)
+
+        return True
+
+    def modelUpdate(self, butler, task_args):
+        arm_id = task_args['arm_id']
+        reward = task_args['reward']
+        participant_uid = task_args['participant_uid']
+
+        d = butler.algorithms.get(key='d')
+        R = butler.algorithms.get(key='R')
+        S = butler.algorithms.get(key='S')
+        delta = butler.algorithms.get(key='delta')
+        ridge = butler.algorithms.get(key='ridge')
+
+        invVt = np.array(butler.participants.get(uid=participant_uid, key='invVt'))
+        b = np.array(butler.participants.get(uid=participant_uid, key='b'))
+        features = np.load('features.npy')
+        x_invVt_norm = butler.participants.get(uid=participant_uid, key='x_invVt_norm')
+        t = butler.participants.get(uid=participant_uid, key='num_reported_answers')
+
+        xt = features[arm_id, :]
+        u = invVt.dot(xt)
+        invVt -= np.outer(u, u) / (1 + np.inner(xt, u))
+        x_invVt_norm -= np.dot(features, u) ** 2 / (1 + np.inner(xt, u))
+        b += reward * xt
+        theta_hat = invVt.dot(b)
+        utils.debug_print((1 + t / (ridge * d)))
+        sqrt_beta = R * np.sqrt(d * np.log((1 + t / (ridge * d)) / delta)) + np.sqrt(ridge) * S
+        expected_rewards = np.dot(features, theta_hat) + sqrt_beta * np.sqrt(x_invVt_norm)
+
+        butler.participants.set(uid=participant_uid, key='arm_order', value=np.argsort(expected_rewards)[::-1])
+        butler.participants.set(uid=participant_uid, key='invVt', value=invVt)
+        butler.participants.set(uid=participant_uid, key='b', value=b)
+        butler.participants.set(uid=participant_uid, key='x_invVt_norm', value=x_invVt_norm)
+
+        return True
+
+    def getModel(self, butler):
+        return butler.algorithms.get(key=['received_rewards', 'num_reported_answers'])
+
+
---
+++ examples/launch_text_bandits.py
@@ -0,0 +1,173 @@
+"""
+Usage:
+    launch.py <target_filename>
+
+Arguments:
+    target_filename (required) : path to a json file of targets
+
+Options:
+    -h, --help
+
+Example:
+
+    > cd ~/Developer/NEXT/examples/
+    > python launch_bio_image_search.py /Users/aniruddha/Downloads/next_dict.json
+
+"""
+
+from __future__ import print_function
+import os
+import csv
+import sys
+from collections import OrderedDict
+import numpy as np
+import requests
+from pprint import pprint
+import json
+import unicodedata
+
+sys.path.append('../../next/lib')
+from docopt import docopt
+
+
+def launch(targets_filename=None, upload=False):
+
+    with open(targets_filename) as f:
+        target_dictionary = json.load(f)
+
+    targetset = []
+    print(target_dictionary.keys())
+    keys = target_dictionary['index_text'].keys()
+    n = target_dictionary['matrix']['shape'][0]
+    d = target_dictionary['matrix']['shape'][1]
+    print('n = %d' % n)
+    print('d = %d' % d)
+    for key in keys:
+        targetset.append({'target_id': target_dictionary['index_text'][key]['index'],
+                          'primary_type': 'text',
+                          # 'primary_description': '{}'.format(unicodedata.normalize('NFKD', target_dictionary['index_text'][key]['text']).encode('utf8','ignore')),
+                          'primary_description': 'text',
+                          'alt_type': 'text',
+                          'alt_description': 'Document {}'.format(key)})
+
+    # pprint(targetset)
+
+    supported_alg_ids = ['RandomSampling']
+
+    alg_list = []
+    for alg_id in supported_alg_ids:
+        alg_item = {}
+        alg_item['alg_id'] = alg_id
+        alg_item['alg_label'] = alg_id
+        alg_item['test_alg_label'] = alg_id
+        if alg_id == 'EpsilonGreedy':
+            alg_item['params'] = {
+                'epsilon': 0.1,
+                'ridge': 1.0
+            }
+        elif alg_id == 'OFUL':
+            alg_item['params'] = {
+                'ridge': 1.0,
+                'R': 1.0,
+                'S': 1.0,
+                'L': 1.0,
+                'c': 1.0
+            }
+        elif alg_id == 'QOFUL':
+            alg_item['params'] = {
+                'ridge': 1.0,
+                'R': 1.0,
+                'S': 1.0,
+                'L': 1.0,
+                'c1': 1.0
+            }
+        elif alg_id == 'TS':
+            alg_item['params'] = {
+                'ridge': 1.0,
+                'R': 1.0,
+                'multiplier': 1.0
+            }
+        elif alg_id == 'GLOC':
+            S = 1.0
+            kappa = 1 / ((1 + np.exp(S)) * (1 + np.exp(-S)))
+            L = 0.25
+            cR = 0.5
+            alg_item['params'] = {
+                'ridge': 1.0,
+                'R': cR,
+                'S': S,
+                'L': L,
+                'kappa': kappa,
+                'c': 0.1
+            }
+        # alg_item['params'] = {}
+        alg_list.append(alg_item)
+
+    algorithm_management_settings = {}
+    params = []
+    for algorithm in alg_list:
+        params += [{'alg_label': algorithm['alg_label'],
+                    'proportion': 1.0 / len(alg_list)}]
+
+    algorithm_management_settings['mode'] = 'fixed_proportions'
+    algorithm_management_settings['params'] = params
+    # Create experiment dictionary
+    initExp = {}
+    initExp['app_id'] = 'TextBandits'
+    initExp['args'] = {}
+    initExp['args']['failure_probability'] = 0.05
+    initExp['args']['participant_to_algorithm_management'] = 'one_to_one'
+    initExp['args']['algorithm_management_settings'] = algorithm_management_settings
+    initExp['args']['alg_list'] = alg_list
+    initExp['args']['num_tries'] = 50
+    initExp['args']['d'] = d
+    initExp['args']['n'] = n
+    initExp['args']['targets'] = {'targetset': targetset}
+    initExp['args']['instructions'] = 'Is this texts similar to previous texts? (Yes/No)'
+
+    initExp['args']['matrix'] = target_dictionary['matrix']
+    initExp['args']['name_to_index_dict'] = target_dictionary['name_to_index_dict']
+    # initExp['args'] = {
+    #     'failure_probability': 0.05,
+    #     'participant_to_algorithm_management': 'one_to_one',
+    #     'algorithm_management_settings': algorithm_management_settings,
+    #     'alg_list': alg_list,
+    #     'num_tries': 50,
+    #     'd': d,
+    #     'n': n,
+    #     'targets': {'targetset': targetset}
+    # }
+
+    # initExp['args']['R'] = 1.
+    # initExp['args']['rating_scale'] = {'labels': [{'label':'yes', 'reward': 1},
+    #                                              {'label':'no', 'reward': -1}]}
+    # with open('initExp.json', 'w') as f:
+    #     json.dump(initExp, f)
+
+    # print(initExp['args']['targets']['matrix'])
+
+    host_url = os.environ.get('NEXT_BACKEND_GLOBAL_HOST', 'localhost')
+    host_url = 'http://' + host_url + ':8000'
+
+    print("Host URL: %s"%host_url)
+    print('Initializing experiment')
+    response = requests.post(host_url + '/api/experiment',
+                             json.dumps(initExp),
+                             headers={'content-type': 'application/json'})
+    # response = requests.post(host_url + '/assistant/init/experiment',
+    #                          json.dumps(initExp),
+    #                          headers={'content-type':'application/json'})
+
+    initExp_response_dict = json.loads(response.text)
+    print('initExp_response_dict', initExp_response_dict)
+    print('Experiment link:')
+    print(host_url + '/query/query_page/landing_text/' + initExp_response_dict['exp_uid'])
+    exp_uid = initExp_response_dict['exp_uid']
+
+if __name__ == "__main__":
+    args = docopt(__doc__)
+    upload = False
+    print(args['<target_filename>'])
+
+    print(args, '\n')
+    launch(targets_filename=args['<target_filename>'], upload=upload)
